be80be4bd1e8e5169704d696753e4449a9fffa1a
Prakash Rajendran <rprakashpb2007@gmail.com>
2025-01-06 18:25:39 +0900

    Update Check_Onedrive_Owner.ps1

--- Check_Onedrive_Owner.ps1
+++ Check_Onedrive_Owner.ps1
@@ -3,5 +3,5 @@ $siteURL = "https://"
 $username = ""
 $password = ""
 $cred = New-Object -TypeName System.Management.Automation.PSCredential -argumentlist $userName, $(convertto-securestring $Password -asplaintext -force)
-Connect-SPOService -Url https://nissangroup-admin.sharepoint.com -credential $cred
+Connect-SPOService -Url https://sitename-admin.sharepoint.com -credential $cred
 Get-SPOSite  $siteURL | Select Owner 
